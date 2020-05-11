5640a9ca542fc19f97bff7a5860e2eaba7295615
Ali Murtaza <a.murtuza007@gmail.com>
2020-05-11 20:28:04 +0500

    get and post method in profiles api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 # Create your views here.
+from profiles_api import serializers
 
 class HelloAPiView(APIView):
+    serializer_class = serializers.HelloSerializer
+
     def get(self,request,format=None):
         api_list_view =[
             'uses Http method as function (post, get, put, patch, delete)',
@@ -12,3 +16,16 @@ class HelloAPiView(APIView):
             'Is mapped usually manually to urls'
         ]
         return Response({'message': 'hello', 'api_list_view': api_list_view})
+
+    def post(self,request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f"hello {name}"
+            return Response({'message':message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+                )
